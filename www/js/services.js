6b491a9f203bf3bca9a6d020f384abb526bf1b9e
xujianrj <keys203@gmail.com>
2014-02-17 18:10:46 +0800

    init

--- www/js/services.js
+++ www/js/services.js
@@ -5,14 +5,20 @@ angular.module('starter.services', [])
  */
 .factory('PetService', function() {
   // Might use a resource here that returns a JSON array
+        var pets=null;
+        var dataRef = new Firebase('https://artsmesh.firebaseio.com/groups');
+        dataRef.on('value', function(snapshot) {
+            pets=snapshot.val();
+        });
 
   // Some fake testing data
-  var pets = [
-    { id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    { id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
-    { id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
-  ];
+
+//  var pets = [
+//    { id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
+//    { id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
+//    { id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
+//    { id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
+//  ];
 
   return {
     all: function() {
